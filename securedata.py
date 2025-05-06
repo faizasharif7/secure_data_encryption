c4441589acce40791a20dcb244ef1bac8242d9ae
faiza sharif <161868234+faizasharif7@users.noreply.github.com>
2025-05-07 01:27:54 +0500

    securedata.py

--- securedata.py
+++ securedata.py
@@ -83,7 +83,7 @@ elif choice == "Register":
         else:
             st.error("Both fields are required.")
 
- elif choice == "Login":
+elif choice == "Login":
         st.subheader("🔑 User Login")
 
         if time.time() < st.session_state.lockout_time:
